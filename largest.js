ccd58df12187d83ae8781e7031c2a08759fbf21e
karanam sai phani teja <43602536+saiphani1702@users.noreply.github.com>
2018-10-02 16:53:25 +0530

    Add files via upload

---
+++ largest.js
@@ -0,0 +1,19 @@
+//declaring and initialisation of variables
+var num1=-8;
+var num2=12.5;
+var num3=-6.769;
+var num4=0;
+var num5=1;
+largest=num1;
+//comparing using conditionals
+if(num2>largest)
+   largest=num2;
+if(num3>largest)
+   largest=num3;
+if(num4>largest)
+   largest=num4;
+if(num5>largest)
+   largest=num5;
+//prints the result to the console
+console.log("The largest number is "+largest);
+
